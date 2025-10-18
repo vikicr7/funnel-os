22b80599265c2188ac2a28b1d7bb5d997175c540
root <root@ip-172-31-33-222.ap-south-1.compute.internal>
2025-10-18 13:06:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Hello from Funnel OS!'));
+app.listen(port, () => console.log(Running on ${port}));
